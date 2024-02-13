0c188e642c7699285e4f24c63817357aca0096d1
CtFatalv <contact.fatalv@gmail.com>
2024-02-13 16:43:33 +0100

    Legacy City

--- resources/[ajout]/[Identite]/um-idcard/fxmanifest.lua
+++ resources/[ajout]/[Identite]/um-idcard/fxmanifest.lua
@@ -4,10 +4,15 @@ lua54 'yes'
 
 name 'um-idcard'
 author 'uyuyorum {um}'
-version '1.2.7'
+version '1.2.8'
 license 'GPL-3.0 license'
 repository 'https://github.com/alp1x/um-idcard'
 description 'FiveM Identity Card for QBCore and ESX and QBox'
+
+dependencies {
+	'ox_lib',
+	'MugShotBase64'
+}
 
 shared_scripts {
 	'@ox_lib/init.lua',
@@ -18,13 +23,12 @@ client_script 'main/client.lua'
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
-	'bridge/framework/*.lua',
-	'bridge/inventory/*.lua',
+	'bridge/**/*.lua',
 	'main/server.lua',
 	'main/version.lua',
 }
 
-ui_page "web/index.html"
+ui_page 'web/index.html'
 
 files {
 	'web/index.html',
